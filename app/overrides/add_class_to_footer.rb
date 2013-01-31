3cc12fe009f8bd886f7c021b152ad5a33a6ebf6a
Tyler Rick <tyler.rick@k3integrations.com>
2013-01-30 19:59:45 -0800

    Lovely. Even though those changes caused it to match 1 times instead of 0 times, it actually messes up the layout. So apparently the css has already been adjusted to work *without* matching .container.
    Deface: 'add_class_to_header' matched 1 times with '#header'

--- app/overrides/add_class_to_footer.rb
+++
@@ -1,6 +0,0 @@
-# TODO: Change .container to #footer in the stylesheets
-Deface::Override.new(:virtual_path => 'spree/shared/_footer',
-                     :name => 'add_class_to_footer',
-                     :set_attributes => '#footer[data-hook]',
-                     :attributes => { :class => 'container' })
-
